b79f6470c2003bb96ec7080cda7c47f3e78012f6
sourabh kumar <sourabh.constacloud@gmail.com>
2024-05-20 20:03:04 +0530

    Leet code 392 is a subsequence

---
+++  IsSubsequence.java
@@ -0,0 +1,37 @@
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+
+
+package leetcode.Leetcode;
+
+class  isSubsequence {
+ 
+    public static void main(String[] args) {
+       String s = "abc";
+        String t = "ahbgdc";
+        int sindex=0;
+        int tindex= 0;
+        while (sindex < s.length() && tindex < tindex) {
+
+            if(s.charAt(sindex) == t.charAt(tindex)){
+                sindex++;
+            }
+            tindex++;
+            
+        }
+        System.out.println((sindex == s.length()));
+    }
+    
+}
